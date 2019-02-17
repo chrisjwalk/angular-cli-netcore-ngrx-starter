eb31fd41c83c9902143f37dce0222c1ea54d8e89
Christopher J. Walker <8442112+chrisjwalk@users.noreply.github.com>
2019-02-17 11:19:14 -0500

    Update packages. Small tweaks copied from new .net core 2.2 app.

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ namespace AngularCliNetcoreNgrxStarter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -49,6 +49,7 @@ namespace AngularCliNetcoreNgrxStarter
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             
