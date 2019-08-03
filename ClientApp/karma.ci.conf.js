a3f86d6827d4041479688b9aa765823de3369b7f
Christopher J. Walker <8442112+chrisjwalk@users.noreply.github.com>
2019-08-03 13:03:31 -0400

    Add cobertura code coverage report for Azure DevOps

--- ClientApp/karma.ci.conf.js
+++ ClientApp/karma.ci.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage'),
-      reports: ['html', 'lcovonly'],
+      reports: ['html', 'lcovonly', 'cobertura'],
       fixWebpackSourcePaths: true
     },
     junitReporter: {
