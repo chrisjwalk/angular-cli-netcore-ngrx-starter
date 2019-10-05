4b63fd828642d68e49443881f2069ed6702bb0fa
Christopher J. Walker <8442112+chrisjwalk@users.noreply.github.com>
2019-10-05 14:13:30 -0400

    Update to dotnet core 3.0

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace AngularCliNetcoreNgrxStarter.Controllers
 {
+    [ApiController]
     [Route("api/[controller]")]
-    public class SampleDataController : Controller
+    public class SampleDataController : ControllerBase
     {
-        private static string[] Summaries = new[]
+        private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
+
+        private readonly ILogger<SampleDataController> _logger;
+
+        public SampleDataController(ILogger<SampleDataController> logger)
+        {
+            _logger = logger;
+        }
 
         [HttpGet("[action]")]
         public IEnumerable<WeatherForecast> WeatherForecasts(int count = 10)
