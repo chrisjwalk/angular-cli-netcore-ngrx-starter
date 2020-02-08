bcbd5115c3a2cfc3ec37c2a9e7234aa385d81675
Christopher J. Walker <8442112+chrisjwalk@users.noreply.github.com>
2020-02-08 11:41:56 -0500

    ngrx router fixes

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreRouterConnectingModule, RouterState } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
@@ -23,9 +23,14 @@ import { environment } from 'environments/environment';
     AppRoutingModule,
     StoreModule.forRoot(reducers, {
       metaReducers,
-      runtimeChecks: { strictStateImmutability: true, strictActionImmutability: true },
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        strictStateSerializability: true,
+        strictActionSerializability: true,
+      },
     }),
-    StoreRouterConnectingModule.forRoot(),
+    StoreRouterConnectingModule.forRoot({ routerState: RouterState.Minimal }),
     EffectsModule.forRoot(effects),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     ServiceWorkerModule.register('/ngsw-worker.js', {
