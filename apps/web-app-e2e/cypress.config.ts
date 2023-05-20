88194c3b20f71d60fc099fdbf72707558d8f519c
chrisjwalk <8442112+chrisjwalk@users.noreply.github.com>
2023-05-20 10:34:30 -0400

    Fix e2e tests

--- apps/web-app-e2e/cypress.config.ts
+++ apps/web-app-e2e/cypress.config.ts
@@ -1,29 +1,6 @@
-import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
+import { nxE2EPreset } from "@nx/cypress/plugins/cypress-preset";
 
-import setupNodeEvents from './src/plugins/index';
-
-const cypressJsonConfig = {
-  fileServerFolder: '.',
-  fixturesFolder: './src/fixtures',
-  video: true,
-  videosFolder: '../../dist/cypress/apps/web-app-e2e/videos',
-  screenshotsFolder: '../../dist/cypress/apps/web-app-e2e/screenshots',
-  chromeWebSecurity: false,
-  specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
-  supportFile: 'src/support/e2e.ts',
-};
 export default defineConfig({
-  e2e: {
-    ...nxE2EPreset(__filename),
-    ...cypressJsonConfig,
-    setupNodeEvents,
-    /**
-     * TODO(@nx/cypress): In Cypress v12,the testIsolation option is turned on by default.
-     * This can cause tests to start breaking where not indended.
-     * You should consider enabling this once you verify tests do not depend on each other
-     * More Info: https://docs.cypress.io/guides/references/migration-guide#Test-Isolation
-     **/
-    testIsolation: false,
-  },
+  e2e: nxE2EPreset(__dirname),
 });
