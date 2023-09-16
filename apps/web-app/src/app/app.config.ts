4cf8ef6ba4bc1982337a18fce86b413cea9fac97
chrisjwalk <8442112+chrisjwalk@users.noreply.github.com>
2023-09-16 10:38:49 -0400

    Use provideAnimations() instead of BrowserAnimationsModule

--- apps/web-app/src/app/app.config.ts
+++ apps/web-app/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
@@ -15,8 +15,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideRouter(routes, withEnabledBlockingInitialNavigation()),
+    provideAnimations(),
     importProvidersFrom(
-      BrowserAnimationsModule,
       MatSnackBar,
       ServiceWorkerModule.register('/ngsw-worker.js', {
         enabled: environment.production,
