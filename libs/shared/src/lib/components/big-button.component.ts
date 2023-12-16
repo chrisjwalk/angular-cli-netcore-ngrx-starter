5f70ec42315e47ad85bfe700acb3e34b56acef43
chrisjwalk <8442112+chrisjwalk@users.noreply.github.com>
2023-12-15 19:09:38 -0500

    Add unit test

--- libs/shared/src/lib/components/big-button.component.ts
+++ libs/shared/src/lib/components/big-button.component.ts
@@ -6,7 +6,11 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [MatButtonModule],
   selector: 'lib-big-button',
   template: `
-    <button mat-button class="!text-base/10 !bg-neutral-900">
+    <button
+      data-testid="lib-big-button"
+      mat-button
+      class="!text-base/10 !bg-neutral-900"
+    >
       <ng-content />
     </button>
   `,
