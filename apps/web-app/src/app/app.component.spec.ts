433127ee4fa1e0dfecaef487335099797c3a9ed1
chrisjwalk <8442112+chrisjwalk@users.noreply.github.com>
2024-01-01 11:27:16 -0500

    Remove environment files

--- apps/web-app/src/app/app.component.spec.ts
+++ apps/web-app/src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideLocationMocks } from '@angular/common/testing';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
 import { render, screen } from '@testing-library/angular';
 
-import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
--- apps/web-app/src/app/app.config.ts
+++ apps/web-app/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   provideRouter,
@@ -8,7 +8,6 @@ import {
 import { provideServiceWorker } from '@angular/service-worker';
 import { authInterceptor } from '@myorg/auth';
 
-import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -17,7 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withEnabledBlockingInitialNavigation()),
     provideAnimations(),
     provideServiceWorker('/ngsw-worker.js', {
-      enabled: environment.production,
+      enabled: !isDevMode(),
     }),
   ],
 };
