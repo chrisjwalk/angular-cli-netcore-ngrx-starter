fc4b94a8d5ffc6fe1dccb7a3ff8a0cbc77068cc2
chrisjwalk <8442112+chrisjwalk@users.noreply.github.com>
2024-02-11 11:57:13 -0500

    Revert tests to old form

--- libs/counter/src/lib/components/counter/counter.component.spec.ts
+++ libs/counter/src/lib/components/counter/counter.component.spec.ts
@@ -1,17 +1,8 @@
-import { Component } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
-import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { fireEvent, render, screen } from '@testing-library/angular';
 
 import { CounterComponent } from './counter.component';
 
 describe('CounterComponent', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [provideNoopAnimations()],
-    });
-  });
-
   test('should exist', async () => {
     await render(CounterComponent);
 
@@ -20,16 +11,10 @@ describe('CounterComponent', () => {
 
   test('should render counter', async () => {
     const count = 5;
-    @Component({
-      standalone: true,
-      imports: [CounterComponent],
-      template: '<lib-counter [count]="count" />',
-    })
-    class TestComponent {
-      count = count;
-    }
+    await render(CounterComponent, {
+      componentInputs: { count },
+    });
 
-    TestBed.createComponent(TestComponent).detectChanges();
     expect(screen.getByTestId('count').innerHTML?.trim()).toBe(`${count}`);
   });
 
@@ -37,17 +22,11 @@ describe('CounterComponent', () => {
     const count = 5;
     const increment = jest.fn();
 
-    @Component({
-      standalone: true,
-      imports: [CounterComponent],
-      template: '<lib-counter [count]="count" (increment)="increment()" />',
-    })
-    class TestComponent {
-      count = count;
-      increment = increment;
-    }
+    await render(CounterComponent, {
+      componentInputs: { count },
+      componentOutputs: { increment: { emit: increment } as any },
+    });
 
-    TestBed.createComponent(TestComponent).detectChanges();
     fireEvent.click(screen.getByText('keyboard_arrow_right'));
     expect(increment).toHaveBeenCalledTimes(1);
   });
--- libs/weather-forecast/src/lib/components/forecast-table/forecast-table.component.spec.ts
+++ libs/weather-forecast/src/lib/components/forecast-table/forecast-table.component.spec.ts
@@ -1,35 +1,19 @@
-import { TestBed } from '@angular/core/testing';
-import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { render, screen } from '@testing-library/angular';
 
 import { weatherForecasts } from '../../services/weather-forecast.service.spec';
 import { ForecastTableComponent } from './forecast-table.component';
-import { Component } from '@angular/core';
 
 describe('ForecastTableComponent', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [provideNoopAnimations()],
-    });
-  });
-
   test('should exist', async () => {
     await render(ForecastTableComponent);
 
     expect(screen.getByTestId('lib-forecast-table')).toBeTruthy();
   });
 
-  test('should have the correct number of rows', () => {
-    @Component({
-      standalone: true,
-      imports: [ForecastTableComponent],
-      template: '<lib-forecast-table [data]="data" />',
-    })
-    class TestComponent {
-      data = weatherForecasts;
-    }
-
-    TestBed.createComponent(TestComponent).detectChanges();
+  test('should have the correct number of rows', async () => {
+    await render(ForecastTableComponent, {
+      componentInputs: { data: weatherForecasts },
+    });
 
     expect(screen.getAllByTestId('table-row')).toHaveLength(
       weatherForecasts.length,
