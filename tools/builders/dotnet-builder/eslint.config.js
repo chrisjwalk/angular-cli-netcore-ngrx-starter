0f09539dc5b26a5d81e2d501faabdff9b319727d
chrisjwalk <8442112+chrisjwalk@users.noreply.github.com>
2024-06-22 16:32:19 -0400

    Lint fix

---
+++ tools/builders/dotnet-builder/eslint.config.js
@@ -0,0 +1,19 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const baseConfig = require('../../../eslint.config.js');
+const js = require('@eslint/js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  ...baseConfig,
+  ...compat.config().map((config) => ({
+    ...config,
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      ...config.rules,
+    },
+  })),
+];
