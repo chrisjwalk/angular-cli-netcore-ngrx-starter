e9b92808df6520cab62d3b8f9b41630d5cdd1e52
chrisjwalk <8442112+chrisjwalk@users.noreply.github.com>
2024-10-20 11:48:36 -0400

    Simplify breakpoint store

--- libs/shared/src/lib/state/breakpoint.store.ts
+++ libs/shared/src/lib/state/breakpoint.store.ts
@@ -7,114 +7,83 @@ import { inject } from '@angular/core';
 import {
   patchState,
   signalStore,
+  signalStoreFeature,
   withHooks,
   withMethods,
   withState,
 } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
-import { combineLatest, debounceTime, map, pipe } from 'rxjs';
+import { map, pipe, tap } from 'rxjs';
 
-export const BreakpointStore = signalStore(
-  withState({
-    xsmall: false,
-    small: false,
-    medium: false,
-    large: false,
-    xlarge: false,
-    handset: false,
-    tablet: false,
-    web: false,
-    handsetPortrait: false,
-    tabletPortrait: false,
-    webPortrait: false,
-    handsetLandscape: false,
-    tabletLandscape: false,
-    webLandscape: false,
-  }),
-  withMethods((store) => ({
-    rxBreakpointObserver: rxMethod<{
-      xsmall: BreakpointState;
-      small: BreakpointState;
-      medium: BreakpointState;
-      large: BreakpointState;
-      xlarge: BreakpointState;
-      handset: BreakpointState;
-      tablet: BreakpointState;
-      web: BreakpointState;
-      handsetPortrait: BreakpointState;
-      tabletPortrait: BreakpointState;
-      webPortrait: BreakpointState;
-      handsetLandscape: BreakpointState;
-      tabletLandscape: BreakpointState;
-      webLandscape: BreakpointState;
-    }>(
-      pipe(
-        debounceTime(0),
-        map(
-          ({
-            xsmall,
-            small,
-            medium,
-            large,
-            xlarge,
-            handset,
-            tablet,
-            web,
-            handsetPortrait,
-            tabletPortrait,
-            webPortrait,
-            handsetLandscape,
-            tabletLandscape,
-            webLandscape,
-          }) =>
+export function withBreakpointFeature() {
+  return signalStoreFeature(
+    withState({
+      xsmall: false,
+      small: false,
+      medium: false,
+      large: false,
+      xlarge: false,
+      handset: false,
+      tablet: false,
+      web: false,
+      handsetPortrait: false,
+      tabletPortrait: false,
+      webPortrait: false,
+      handsetLandscape: false,
+      tabletLandscape: false,
+      webLandscape: false,
+    }),
+    withMethods((store) => ({
+      rxBreakpointObserver: rxMethod<BreakpointState>(
+        pipe(
+          map((result) => result.breakpoints),
+          tap((matches) =>
             patchState(store, {
-              xsmall: xsmall.matches,
-              small: small.matches,
-              medium: medium.matches,
-              large: large.matches,
-              xlarge: xlarge.matches,
-              handset: handset.matches,
-              tablet: tablet.matches,
-              web: web.matches,
-              handsetPortrait: handsetPortrait.matches,
-              tabletPortrait: tabletPortrait.matches,
-              webPortrait: webPortrait.matches,
-              handsetLandscape: handsetLandscape.matches,
-              tabletLandscape: tabletLandscape.matches,
-              webLandscape: webLandscape.matches,
+              xsmall: matches[Breakpoints.XSmall],
+              small: matches[Breakpoints.Small],
+              medium: matches[Breakpoints.Medium],
+              large: matches[Breakpoints.Large],
+              xlarge: matches[Breakpoints.XLarge],
+              handset:
+                matches[Breakpoints.HandsetPortrait] ||
+                matches[Breakpoints.HandsetLandscape],
+              tablet:
+                matches[Breakpoints.TabletPortrait] ||
+                matches[Breakpoints.TabletLandscape],
+              web:
+                matches[Breakpoints.WebPortrait] ||
+                matches[Breakpoints.WebLandscape],
+              handsetPortrait: matches[Breakpoints.HandsetPortrait],
+              tabletPortrait: matches[Breakpoints.TabletPortrait],
+              webPortrait: matches[Breakpoints.WebPortrait],
+              handsetLandscape: matches[Breakpoints.HandsetLandscape],
+              tabletLandscape: matches[Breakpoints.TabletLandscape],
+              webLandscape: matches[Breakpoints.WebLandscape],
             }),
+          ),
         ),
       ),
-    ),
-  })),
-  withHooks((store, breakpointObserver = inject(BreakpointObserver)) => ({
-    onInit() {
-      store.rxBreakpointObserver(
-        combineLatest({
-          xsmall: breakpointObserver.observe(Breakpoints.XSmall),
-          small: breakpointObserver.observe(Breakpoints.Small),
-          medium: breakpointObserver.observe(Breakpoints.Medium),
-          large: breakpointObserver.observe(Breakpoints.Large),
-          xlarge: breakpointObserver.observe(Breakpoints.XLarge),
-          handset: breakpointObserver.observe(Breakpoints.Handset),
-          tablet: breakpointObserver.observe(Breakpoints.Tablet),
-          web: breakpointObserver.observe(Breakpoints.Web),
-          handsetPortrait: breakpointObserver.observe(
+    })),
+    withHooks((store, breakpointObserver = inject(BreakpointObserver)) => ({
+      onInit() {
+        store.rxBreakpointObserver(
+          breakpointObserver.observe([
+            Breakpoints.XSmall,
+            Breakpoints.Small,
+            Breakpoints.Medium,
+            Breakpoints.Large,
+            Breakpoints.XLarge,
             Breakpoints.HandsetPortrait,
-          ),
-          tabletPortrait: breakpointObserver.observe(
             Breakpoints.TabletPortrait,
-          ),
-          webPortrait: breakpointObserver.observe(Breakpoints.WebPortrait),
-          handsetLandscape: breakpointObserver.observe(
+            Breakpoints.WebPortrait,
             Breakpoints.HandsetLandscape,
-          ),
-          tabletLandscape: breakpointObserver.observe(
             Breakpoints.TabletLandscape,
-          ),
-          webLandscape: breakpointObserver.observe(Breakpoints.WebLandscape),
-        }),
-      );
-    },
-  })),
-);
+            Breakpoints.WebLandscape,
+          ]),
+        );
+      },
+    })),
+  );
+}
+
+export const BreakpointStore = signalStore(withBreakpointFeature());
