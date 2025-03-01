57505f7bfc3b9a56696b43aa8d2724c1869738b0
chrisjwalk <8442112+chrisjwalk@users.noreply.github.com>
2025-03-01 11:15:26 -0500

    Update to talwind 4

--- libs/shared/src/lib/components/page-toolbar.component.ts
+++ libs/shared/src/lib/components/page-toolbar.component.ts
@@ -9,20 +9,6 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
     <span class="flex-1"></span>
     <ng-content />
   `,
-  styles: [
-    `
-      :host ::ng-deep {
-        .mat-mdc-form-field-subscript-wrapper {
-          @apply hidden;
-        }
-
-        .mat-mdc-text-field-wrapper.mdc-text-field--outlined
-          .mat-mdc-form-field-infix {
-          @apply py-1 min-h-[34px];
-        }
-      }
-    `,
-  ],
   host: {
     class:
       'bg-white/95 dark:bg-neutral-700 flex flex-row whitespace-nowrap gap-2 py-0 px-4 box-border items-center h-[46px]',
